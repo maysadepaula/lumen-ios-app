de23fa3be82524a3c6cf5bbc7d6c0ccf5e00d4c3
Maysa Angélica <maysa.dpaulas@gmail.com>
2025-10-03 11:27:39 -0300

    Initial Commit

---
+++ Lumen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lumen
+//
+//  Created by Maysa Fonseca on 03/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lumen/LumenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumenApp.swift
+//  Lumen
+//
+//  Created by Maysa Fonseca on 03/10/25.
+//
+
+import SwiftUI
+
+@main
+struct LumenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
